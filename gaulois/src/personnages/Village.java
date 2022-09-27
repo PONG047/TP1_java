16555618cda830f7d56da06d9a2f3bf5e638b965
etudianfsi <etudiantfsi@ens.fsi>
2022-09-27 19:55:48 +0200

    V1

---
+++ gaulois/src/personnages/Village.java
@@ -0,0 +1,17 @@
+package personnages;
+
+public class Village {
+	private String nom;
+	private Chef chef;
+	
+	
+	public Village(String nom) {
+		super();
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+}
